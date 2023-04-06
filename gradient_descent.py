5cbdfb328d5d751b75acc3c7086e138dda30324f
Ion Gradea <iongradea@MacBook-Pro-de-Ion.local>
2023-04-06 22:16:50 +0200

    works v1.0 with minmax normalization

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,10 +1,10 @@
-def gradient_descent(x, y, theta0, theta1, alpha, num_iters):
+def gradient_descent(x, y, theta0, theta1, alpha, num_iters, J):
     m = len(y)  # number of training examples
 
     # Initialize theta0 and theta1 as lists with the initial values
     theta0 = [theta0]
     theta1 = [theta1]
-    J = []  # cost function initialization
+    J = [J]  # cost function initialization
 
     for el in range(num_iters):
         h = [theta0[-1] + theta1[-1] * x_i for x_i in x]  # hypothesis
@@ -12,7 +12,7 @@ def gradient_descent(x, y, theta0, theta1, alpha, num_iters):
         error = [h_i - y_i for h_i, y_i in zip(h, y)]  # prediction error
 
         theta0.append(theta0[-1] - alpha / m * sum(error))
-        theta1.append(theta1[-1] - alpha / m * sum(e * x_i for e, x_i in zip(error, x)))
+        theta1.append(theta1[-1] - alpha / m * sum(e_i * x_i for e_i, x_i in zip(error, x)))
 
         J.append(1 / (2 * m) * sum(e_i ** 2 for e_i in error))
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from gradient_descent import *
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 def read_csv_file(filename):
     with open(filename, 'r') as csvfile:
@@ -21,8 +22,16 @@ def plot_graph(x_km, y_price, y):
     plt.xlabel('X km')
     plt.ylabel('Y price')
     plt.title('XY km/price')
-    plt.scatter(x_km, y_price)
-    plt.scatter(x_km, y)
+    plt.scatter(x_km, y_price, marker="x")
+    plt.scatter(x_km, y, marker="o")
+    plt.show()
+
+def plot_cost_function(J):
+    plt.xlabel('Number of iterations')
+    plt.ylabel('Cost function')
+    plt.title('Cost function')
+    iterations = list(range(len(J)))
+    plt.plot(iterations, J)
     plt.show()
 
 def convert_list_to_float(list):
@@ -30,17 +39,40 @@ def convert_list_to_float(list):
         list[el] = float(list[el])
     return list
 
+def normalize_minmax_list(list):
+    min_el = min(list)
+    max_el = max(list)
+    list = [(x_i - min_el)/(max_el - min_el) for x_i in list]
+    return list
+
+def normalize_zscore_list(list):
+    list_mean = np.mean(list, axis=0)
+    list_std = np.std(list, axis=0)
+    list_normalized = (list - list_mean) / list_std
+    return list_normalized, list_mean, list_std
+
 def main():
     x_km, y_price = read_csv_file('data.csv')
     x_km = convert_list_to_float(x_km)
     y_price = convert_list_to_float(y_price)
-    theta0, theta1, J = gradient_descent(x_km, y_price, 0, 0, 0.0000000001, 1000000)
+    x_km = normalize_minmax_list(x_km)
+    y_price = normalize_minmax_list(y_price)
+    # x_km = normalize_zscore_list(x_km)
+    # y_price = normalize_zscore_list(y_price)
+    print(x_km)
+    print(y_price)
+    # theta0, theta1, J = gradient_descent(x_km, y_price, 0, 0, 0.0001, 10000000)
+    # theta0, theta1, J = gradient_descent(x_km, y_price, 0, 0, 0.001, 1000000)
+    theta0, theta1, J = gradient_descent(x_km, y_price, 0, 0, 0.001, 100000, 0)
     print(len(theta0))
     print(J[-1])
+    print(theta0[-1])
     print(theta1[-1])
-    print(type(theta0[-1]))
-    y = [theta0[-1] - theta1[-1] * x_i for x_i in x_km]
+    # print(type(theta0[-1]))
+    y = [theta0[-1] + theta1[-1] * x_i for x_i in x_km]
+
     plot_graph(x_km, y_price, y)
+    plot_cost_function(J)
     # plot_graph(x_km, y)
     
 main()
